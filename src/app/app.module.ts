a7028db0bd2b0e527b1bffb851c713c751961590
mkvinihd <wydm22@gmail.com>
2018-09-16 21:26:37 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculadoraComponent } from './calculadora/components/calculadora.component';
+import { CalculadoraService } from './calculadora/services/calculadora.service';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculadoraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule
+    
   ],
-  providers: [],
+  providers: [CalculadoraService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
---
+++ src/app/calculadora/services/calculadora.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculadoraService {
+
+	// define as constantes utilizadas para identificar
+	// as operações de cálculo
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
+
+  constructor() { }
+
+  calcular(num1: number, num2:number, operacao: string): number {
+  	let resultado: number;//armazena o resultado da operação
+
+  	if(operacao == ''){
+  		alert('Escolha uma operacao');
+  		return 0;
+  	}else{
+	  	switch(operacao){
+	  		case CalculadoraService.SOMA:
+	  			resultado = num1 + num2;
+	  		break;
+	  		case CalculadoraService.SUBTRACAO:
+	  			resultado = num1 - num2;
+	  		break;
+	  		case CalculadoraService.DIVISAO:
+	  			resultado = num1 / num2;
+	  		break;
+	  		case CalculadoraService.MULTIPLICACAO:
+	  			resultado = num1 * num2;
+	  		break;
+	  		default:
+	  			resultado = 0;
+	  	}
+	  		return resultado;
+	  }
+	}
+}
